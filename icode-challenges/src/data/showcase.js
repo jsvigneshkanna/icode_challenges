5d740bf60346ed2e86b7e707894f83652c9ba1ef
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-01-21 17:17:05 +0530

    showcase initial commit

---
+++ icode-challenges/src/data/showcase.js
@@ -0,0 +1,50 @@
+export const showcaseData = [
+  {
+    title: "profile tooltip",
+    github: "/",
+    pageHref: "/profile_tooltip",
+    description:
+      "Interactive profile photo on click/ hove opens corresponding tooltip with details of the user on side of the photo 📸",
+    posterImage: "https://www.icodethis.com/images/projects/profile_popup.png",
+  },
+  {
+    title: "profile tooltip",
+    github: "/",
+    pageHref: "/profile_tooltip",
+    description:
+      "Interactive profile photo on click/ hove opens corresponding tooltip with details of the user on side of the photo 📸",
+    posterImage: "https://www.icodethis.com/images/projects/profile_popup.png",
+  },
+  {
+    title: "profile tooltip",
+    github: "/",
+    pageHref: "/profile_tooltip",
+    description:
+      "Interactive profile photo on click/ hove opens corresponding tooltip with details of the user on side of the photo 📸",
+    posterImage: "https://www.icodethis.com/images/projects/profile_popup.png",
+  },
+  {
+    title: "profile tooltip",
+    github: "/",
+    pageHref: "/profile_tooltip",
+    description:
+      "Interactive profile photo on click/ hove opens corresponding tooltip with details of the user on side of the photo 📸",
+    posterImage: "https://www.icodethis.com/images/projects/profile_popup.png",
+  },
+  {
+    title: "profile tooltip",
+    github: "/",
+    pageHref: "/profile_tooltip",
+    description:
+      "Interactive profile photo on click/ hove opens corresponding tooltip with details of the user on side of the photo 📸",
+    posterImage: "https://www.icodethis.com/images/projects/profile_popup.png",
+  },
+  {
+    title: "profile tooltip",
+    github: "/",
+    pageHref: "/profile_tooltip",
+    description:
+      "Interactive profile photo on click/ hove opens corresponding tooltip with details of the user on side of the photo 📸",
+    posterImage: "https://www.icodethis.com/images/projects/profile_popup.png",
+  },
+];
