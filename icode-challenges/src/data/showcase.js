b59b94c3235b7d60d6c19f248f90acec8376aaef
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-01-21 18:05:10 +0530

    sigin in form solution github link added

--- icode-challenges/src/data/showcase.js
+++ icode-challenges/src/data/showcase.js
@@ -1,7 +1,8 @@
 export const showcaseData = [
   {
     title: "Sign in form",
-    github: "/",
+    github:
+      "https://github.com/jsvigneshkanna/icode_challenges/blob/master/icode-challenges/src/pages/solutions/signin_form.astro",
     pageHref: "/solutions/signin_form",
     description:
       "Interactive popup which describes the sign in/ login feature. We have username, password text input and buttons to signin",
