ba1961be5e70736907f6a5805d138bc15615b358
J S Vignesh Kanna <vkrishitaran@gmail.com>
2023-01-22 17:08:16 +0530

    facebook sign up initial commit

--- icode-challenges/src/data/showcase.js
+++ icode-challenges/src/data/showcase.js
@@ -8,4 +8,12 @@ export const showcaseData = [
       "Interactive popup which describes the sign in/ login feature. We have username, password text input and buttons to signin",
     posterImage: "https://www.icodethis.com/images/projects/sign_in_form.png",
   },
+  {
+    title: "Facebook Login form",
+    github:
+      "https://github.com/jsvigneshkanna/icode_challenges/blob/master/icode-challenges/src/pages/solutions/facebook_login_form.astro",
+    pageHref: "/solutions/facebook_login_form",
+    description: "Facebook Community Joining form which asks the users to give login credentials",
+    posterImage: "https://www.icodethis.com/images/projects/sign_up_form.png",
+  },
 ];
